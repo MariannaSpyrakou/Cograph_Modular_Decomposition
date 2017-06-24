8cb3165e0b00ae07e3ec598f52475bf7310984eb
MariannaSpyrakou <mar.spyrakou@gmail.com>
2017-06-24 21:31:34 +0300

    Update and rename run_file.py to example_1.py

--- example_1.py
+++ example_1.py
@@ -1,4 +1,8 @@
 from Trees import Tree
+
+# add node x that is adjacent to a,f,d,e to the cotree t 
+# output: the updated cotree in postorder tree traversal
+
 t = Tree('1', [Tree('0',[Tree('c'),Tree('1',[Tree('a'),Tree('b')])]),Tree('0',[Tree('d'),Tree('e'),Tree('f')])])
 s=['a','f','d','e']
 flag_mixed=[0]
