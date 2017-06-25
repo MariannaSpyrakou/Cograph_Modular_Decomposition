5331c93162a23cb444ec9eed1bffd66287c2d78d
MariannaSpyrakou <mar.spyrakou@gmail.com>
2017-06-25 12:30:00 +0300

    Update create_cotree.py

--- create_cotree.py
+++ create_cotree.py
@@ -1,31 +1,61 @@
 from Trees import Tree
 
-# function create_cotree(g)
-# computes the cotree of the given graph g and prints it (postorder) 
-
 def create_cotree(g):
 	cotree=Tree('1')
+	# the root of the tree is always '1'
 	i=0
 	for node in g:
 		if i==0:
 			first_node=node
+			i=i+1
 		elif i==1:
 			if g[node].count(first_node):
+				# first and second node are adjacent
 				Tree.add_child(cotree,Tree(first_node))
 				Tree.add_child(cotree,Tree(node))
 			else:
+				# first and second node are not adjacent
 				tree0=Tree('0',[Tree(first_node),Tree(node)])
 				Tree.add_child(cotree,tree0)
+			i=i+1
 		else:
+			# add incrementally nodes 3,...,end to the cotree
 			flag_mixed=[0]
 			Tree.is_cograph(cotree,node,g[node],flag_mixed)
 			if flag_mixed[0]==0:
 				Tree.update_cotree(cotree,node,g[node],0,0)
 			elif flag_mixed[0]==2:
-				print "The input graph is not a co-graph"
+				print ("The input graph is not a co-graph. Execution terminated!")
 				return
+			# initialize tree info for the next iteration
 			Tree.make_info_None(cotree)
-		i=i+1
-	
+	Tree.print_tree(cotree)
+	return
+
+def create_cotree(name,neighbors):
+	cotree=Tree('1')
+	# the root of the tree is always '1'
+	for i in range(len(name)):
+		if i==1:
+			if neighbors[1].count(name[0]):
+				# first and second node are adjacent
+				Tree.add_child(cotree,Tree(name[0]))
+				Tree.add_child(cotree,Tree(name[1]))
+			else:
+				# first and second node are not adjacent
+				tree0=Tree('0',[Tree(name[0]),Tree(name[1])])
+				Tree.add_child(cotree,tree0)
+		elif i!=0:
+			# add incrementally nodes 3,...,end to the cotree
+			flag_mixed=[0]
+			Tree.is_cograph(cotree,name[i],neighbors[i],flag_mixed)
+			if flag_mixed[0]==0:
+				Tree.update_cotree(cotree,name[i],neighbors[i],0,0)
+			elif flag_mixed[0]==2:
+				print ("The input graph is not a co-graph. Execution terminated!")
+				return
+			if i<len(name)-1:
+				# initialize tree info for the next iteration
+				Tree.make_info_None(cotree)
 	Tree.print_tree(cotree)
 	return
