728273433cc6c0e495215549bfec44db466babcc
MariannaSpyrakou <mar.spyrakou@gmail.com>
2017-06-25 12:52:47 +0300

    Update main_example.py

--- main_example.py
+++ main_example.py
@@ -1,6 +1,7 @@
 from sage.all import Graph
 from create_cotree import create_cotree
 
+# input option 1:
 d = {'a': ['b','d','e','f','x'],
          'b': ['a','f','e','d'], 'c': ['f','e','d'],
          'd': ['a','b','c','x'], 'e': ['a','b','c','x'],
@@ -8,3 +9,10 @@ d = {'a': ['b','d','e','f','x'],
          }
 g=graph(d)
 create_cotree(g)
+
+
+# input option 2:
+names =['a','b','c','d','e','f','x'] 
+neighbors = [[],['a'] ,[],  ['a','b','c'], ['a','b','c'], ['a','b','c'] , ['f','a','d','e']]
+
+create_cotree(names,neighbors)
